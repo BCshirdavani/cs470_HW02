f8bafc859dd864ed790157034a04d3b8c17a6d92
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-02-04 21:57:11 -0800

    music added
    To-Do:
     - add card game logic...fragments rather than imageView?
     - save points data

--- app/src/main/java/com/example/shymacbook/memorygame/MainActivity.java
+++ app/src/main/java/com/example/shymacbook/memorygame/MainActivity.java
@@ -1,3 +1,15 @@
+//************************************************************
+//
+//  Author:         Beau Shirdavani
+//  Description:    hw02 - Memory Game
+//  Date:           02-04-2018
+//
+//  ToDo:       Ancestral "Up" Button fails
+//  ToDo:       card game child activity incomplete
+//
+//************************************************************
+
+
 package com.example.shymacbook.memorygame;
 
 import android.content.Context;
@@ -5,6 +17,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -15,17 +28,19 @@ public class MainActivity extends AppCompatActivity {
     private Button play;
     private EditText mNameEntry;
     private String nameEntered;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ToDo - up button here fails
 //        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         //media player starts
-//        mp= MediaPlayer.create(this, R.raw.jeopardy);
-//        mp.start();
+        mp= MediaPlayer.create(this, R.raw.jeopardy);
+        mp.start();
 
         play = (Button) findViewById(R.id.PLAYbutton);
         mNameEntry = (EditText) findViewById(R.id.editText);
@@ -49,8 +64,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        // ToDo - up button here fails
 //        getActionBar().setDisplayHomeAsUpEnabled(true);
         return true;
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("testing", "\t\tonPause() was called!");
+        mp.pause();
     }
 
 
